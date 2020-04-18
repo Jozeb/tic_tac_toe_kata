5c42dff4ce4ac87ddfcedfb23ff47a8308fb8058
lamak <lamak.qaizar@careem.com>
2020-04-18 16:58:58 +0500

    REFACTOR: checking outcome via 2d grid

--- src/main/java/TicTacGame.java
+++ src/main/java/TicTacGame.java
@@ -1,6 +1,7 @@
 public class TicTacGame {
 
     State[] grid = new State[10];
+    State[][] grid2d = new State[3][3];
 
     public void playerMove(int i) throws PositionAlreadyOccupiedException {
 
@@ -16,20 +17,46 @@ public class TicTacGame {
         move(State.COMPUTER, i);
     }
 
+    private void move2d(State state, int i) throws PositionAlreadyOccupiedException {
+        int row = (i - 1) / 3;
+        int column = (i - 1) % 3;
+        if (grid2d[row][column] != null) {
+            throw new PositionAlreadyOccupiedException();
+        }
+        grid2d[row][column] = state;
+    }
+
     private void move(State state, int i) throws PositionAlreadyOccupiedException{
         if (grid[i] != null) {
             throw new PositionAlreadyOccupiedException();
         }
         grid[i] = state;
+        move2d(state, i);
+    }
+
+    public String checkOutcome2d() {
+        for (int i = 0; i < 3; i++) {
+            if (grid2d[i][0] == grid2d[i][1] && grid2d[i][1] == grid2d[i][2]) {
+                return grid2d[i][0].getWinningOutcome();
+            }
+
+            if (grid2d[0][i] == grid2d[1][i] && grid2d[1][i] == grid2d[2][i]) {
+                return grid2d[0][i].getWinningOutcome();
+            }
+        }
+
+        if (grid2d[0][0] == grid2d[1][1] && grid2d[1][1] == grid2d[2][2]) {
+            return grid2d[0][0].getWinningOutcome();
+        }
+
+        if (grid2d[0][2] == grid2d[1][1] && grid2d[1][1] == grid2d[2][0]) {
+            return grid2d[0][2].getWinningOutcome();
+        }
+
+        return null;
     }
 
     public String checkOutcome() {
-        if(grid[2] == grid[5] && grid[5] == grid[8])
-            return grid[2].getWinningOutcome();
-        else if(grid[1] == grid[5] && grid[5] == grid[9])
-            return grid[1].getWinningOutcome();
-        else if(grid[3] == grid[5] && grid[5] == grid[7])
-            return grid[3].getWinningOutcome();
-        return State.PLAYER.getWinningOutcome();
+        return checkOutcome2d();
     }
 }
