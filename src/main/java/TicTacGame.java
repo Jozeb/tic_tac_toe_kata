8755608bd649cb14debc20ed3a792f79d19570a2
lamak <lamak.qaizar@careem.com>
2020-04-19 02:31:11 +0500

    REFACTOR: converted if condition to static array

--- src/main/java/TicTacGame.java
+++ src/main/java/TicTacGame.java
@@ -6,6 +6,9 @@ import exception.PositionAlreadyFilledException;
 import exception.WrongMoveException;
 import turn.TurnCheckers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static board.Marker.NOUGHT;
 import static board.Outcome.CROSS_WON;
 import static board.Outcome.NOUGHTS_WON;
@@ -13,8 +16,12 @@ import static board.Position.at;
 
 public class TicTacGame {
 
-    Board board = new Board();
-    TurnCheckers turnCheckers = new TurnCheckers();
+    final Board board = new Board();
+    final TurnCheckers turnCheckers = new TurnCheckers();
+
+    static final List<Position> WINNING_POSITIONS = Arrays.asList(
+            at(1,1), at(1,2), at(1,3)
+    );
 
     public TicTacGame play(Marker marker, Position position) throws WrongMoveException {
         turnCheckers.runAllFor(marker);
@@ -31,21 +38,21 @@ public class TicTacGame {
     }
 
     public Outcome outcome() {
-        Marker marker = whatIs(at(1, 1));
-        if (marker == whatIs(at(1, 2)) && marker == whatIs(at(1,3))) {
+        final Marker marker = whatIs(WINNING_POSITIONS.get(0));
+        if (WINNING_POSITIONS.stream().allMatch(position -> whatIs(position) == marker)) {
             if (marker == NOUGHT) {
                 return NOUGHTS_WON;
             }
         }
-        marker = whatIs(at(2, 1));
-        if (marker == whatIs(at(2, 2)) && marker == whatIs(at(2,3))) {
-            if (marker == NOUGHT) {
+        Marker markerSecondRow = whatIs(at(2, 1));
+        if (markerSecondRow == whatIs(at(2, 2)) && markerSecondRow == whatIs(at(2,3))) {
+            if (markerSecondRow == NOUGHT) {
                 return NOUGHTS_WON;
             }
         }
-        marker = whatIs(at(3, 1));
-        if (marker == whatIs(at(3, 2)) && marker == whatIs(at(3,3))) {
-            if (marker == NOUGHT) {
+        Marker markerThirdRow = whatIs(at(3, 1));
+        if (markerThirdRow == whatIs(at(3, 2)) && markerThirdRow == whatIs(at(3,3))) {
+            if (markerThirdRow == NOUGHT) {
                 return NOUGHTS_WON;
             }
         }
