82722c0d88dc70f1dbbb914c64552f9b80107559
shujaat.ali <=shujaat.dev@gmail.com>
2020-04-18 16:48:12 +0500

    added exception cases

--- src/main/java/TicTacGame.java
+++ src/main/java/TicTacGame.java
@@ -1,23 +1,48 @@
 package com.game.tictactoe;
 
+import exceptions.IllegalMoveException;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class TicTacGame {
 
-  private List<Character> board = new ArrayList<>(9);
+    private List<Character> board = new ArrayList<>(9);
+    private Character lastMove;
 
-  public TicTacGame() {
-    board = Arrays.asList('-', '-', '-', '-', '-', '-', '-', '-', '-');
-  }
+    public TicTacGame() {
+        board = Arrays.asList('-', '-', '-', '-', '-', '-', '-', '-', '-');
+    }
 
-  public void playerMove(int gridNo, Character playerMark) {
-    int index = gridNo - 1;
-    board.set(index, playerMark);
-  }
+    private Character playerX = 'X';
+    private Character playerO = 'O';
 
-  public List<Character> getBoard() {
-    return board;
-  }
+
+    public void playerMove(int gridNo, Character playerMark) throws IllegalMoveException {
+        int index = gridNo - 1;
+        checkPlayerIllegalMoves(playerMark);
+        board.set(index, playerMark);
+        lastMove = playerMark;
+    }
+
+    private void checkPlayerIllegalMoves(Character playerMark) throws IllegalMoveException {
+        if (unknownPlayerMove(playerMark)) {
+            throw new IllegalMoveException("Illegal move:Unknown player made a move");
+        } else if (samePlayerIllegalMoves(playerMark)) {
+            throw new IllegalMoveException("Illegal move:same player made a move  twice in a row");
+        }
+    }
+
+    private boolean samePlayerIllegalMoves(Character playerMark) {
+        return lastMove == playerMark;
+    }
+
+    private boolean unknownPlayerMove(Character playerMark) {
+        return playerMark != playerX && playerMark != playerO;
+    }
+
+    public List<Character> getBoard() {
+        return board;
+    }
 }
--- src/main/java/exceptions/IllegalMoveException.java
+++ src/main/java/exceptions/IllegalMoveException.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+package exceptions;
 
-public class IllegalMoveException {
+public class IllegalMoveException extends Exception{
+    private String message;
+    public IllegalMoveException(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
 }
