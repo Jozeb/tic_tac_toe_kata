e710176b27ec67285e70a78d7c63c931d1908fb2
saad352 <saadabdullahmunir@gmail.com>
2020-04-18 15:38:20 +0500

    first commit

--- src/test/java/TicTacGameTest.java
+++ src/test/java/TicTacGameTest.java
@@ -2,10 +2,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class TicTacGameTest {
 
   @Test
   public void func() {
     TicTacGame ticTacGame = new TicTacGame();
+  }
+
+  @Test
+  public void checkForOverLappingFunc() {
+    List<Integer> playerMove = Arrays.asList(1,5);
+    List<Integer> computerMove = Arrays.asList(3,5);
+
+
   }
 }
