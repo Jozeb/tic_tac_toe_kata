67ccadb7b0fd131c242d52940c89b7d4809cdd3b
muqsit25 <muqsitabbasi@hotmail.com>
2020-04-18 15:48:34 +0500

    commit 1

--- src/test/java/TicTacGameTest.java
+++ src/test/java/TicTacGameTest.java
@@ -5,8 +5,14 @@ import org.junit.Test;
 public class TicTacGameTest {
 
   @Test
-  public void func() {
-
-    //TicTacGame ticTacGame = new TicTacGame();
+  public void GameExists() {
+    TicTacGame ticTacGame = new TicTacGame();
   }
+
+  @Test
+  public void ensurePlayer1MovesFirst() {
+    TicTacGame ticTacGame = new TicTacGame();
+    assertThat(ticTacGame.firstMove)=1;
+  }
+
 }
