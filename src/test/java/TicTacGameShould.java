a62977b02724d2a7d5a371e9cad6101bc2a69a11
lamak <lamak.qaizar@careem.com>
2020-04-19 02:15:41 +0500

    RED: added test case for noughts lining up on third row

--- src/test/java/TicTacGameShould.java
+++ src/test/java/TicTacGameShould.java
@@ -105,4 +105,16 @@ public class TicTacGameShould {
                 .play(NOUGHT, at(2, 3));
         assert ticTacGame.outcome() == Outcome.NOUGHTS_WON;
     }
+
+    @Test
+    public void winWhenNoughtsLinedUpInThirdRow() throws WrongMoveException {
+        TicTacGame ticTacGame = new TicTacGame()
+                .play(CROSS, at(1, 1))
+                .play(NOUGHT, at(3, 1))
+                .play(CROSS, at(1, 2))
+                .play(NOUGHT, at(3, 2))
+                .play(CROSS, at(2, 3))
+                .play(NOUGHT, at(3, 3));
+        assert ticTacGame.outcome() == Outcome.NOUGHTS_WON;
+    }
 }
