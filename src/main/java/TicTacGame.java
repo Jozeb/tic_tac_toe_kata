55e66d94ad0218ee779a509260272c0e9daf0d49
lamak <lamak.qaizar@careem.com>
2020-04-19 00:01:13 +0500

    REFACTOR: moved row and column primitives to position class

--- src/main/java/TicTacGame.java
+++ src/main/java/TicTacGame.java
@@ -1,12 +1,30 @@
 public class TicTacGame {
-    public void playXAt(int row, int column) {
+    public void playXAt(Position position) {
     }
 
-    public boolean isXAt(int row, int column) {
+    public boolean isXAt(Position position) {
         return true;
     }
 
-    public void play0At(int row, int column) throws PositionAlreadyFilledException  {
+    public void play0At(Position position) throws PositionAlreadyFilledException  {
         throw new PositionAlreadyFilledException();
+    }
+
+    static class Position {
+        private final int row;
+        private final int column;
+
+        Position(int row, int column) {
+            this.row = row;
+            this.column = column;
+        }
+
+        public int getRow() {
+            return row;
+        }
+
+        public int getColumn() {
+            return column;
+        }
     }
 }
--- src/test/java/TicTacGameShould.java
+++ src/test/java/TicTacGameShould.java
@@ -4,14 +4,14 @@ public class TicTacGameShould {
     @Test
     public void moveXToCorrectPosition() {
         TicTacGame ticTacGame = new TicTacGame();
-        ticTacGame.playXAt(1, 1);
-        assert ticTacGame.isXAt(1, 1);
+        ticTacGame.playXAt(new TicTacGame.Position(1, 1));
+        assert ticTacGame.isXAt(new TicTacGame.Position(1, 1));
     }
 
     @Test(expected = PositionAlreadyFilledException.class)
     public void disallowMoveToSamePosition() throws PositionAlreadyFilledException {
         TicTacGame ticTacGame = new TicTacGame();
-        ticTacGame.playXAt(1, 1);
-        ticTacGame.play0At(1, 1);
+        ticTacGame.playXAt(new TicTacGame.Position(1, 1));
+        ticTacGame.play0At(new TicTacGame.Position(1, 1));
   }
 }
