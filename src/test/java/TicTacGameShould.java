f57a0d7e5edd1ed45b9d2c7bf744c8eddc8549de
lamak <lamak.qaizar@careem.com>
2020-04-19 05:32:18 +0500

    REFACTOR: removed unused test inits

--- src/test/java/TicTacGameShould.java
+++ src/test/java/TicTacGameShould.java
@@ -1,4 +1,3 @@
-import board.Outcome;
 import exception.PlayCrossException;
 import exception.PlayCrossFirstException;
 import exception.PlayNoughtException;
@@ -7,26 +6,26 @@ import exception.WrongMoveException;
 import org.junit.Test;
 
 import static board.Marker.CROSS;
-import static board.Marker.NOUGHT;
 import static board.Marker.EMPTY;
+import static board.Marker.NOUGHT;
 import static board.Outcome.CROSSES_WON;
-import static board.Outcome.NOUGHTS_WON;
 import static board.Outcome.IN_PROGRESS;
+import static board.Outcome.NOUGHTS_WON;
 import static board.Position.at;
 
 public class TicTacGameShould {
 
     @Test
     public void indicateEmptyPosition() {
-        TicTacGame ticTacGame = new TicTacGame();
-        ticTacGame.assertThat(EMPTY, at(1, 1));
+        new TicTacGame()
+                .assertThat(EMPTY, at(1, 1));
     }
 
     @Test
     public void playCross() throws WrongMoveException {
-        TicTacGame ticTacGame = new TicTacGame()
-                .play(CROSS, at(1, 1));
-        ticTacGame.assertThat(CROSS, at(1, 1));
+        new TicTacGame()
+                .play(CROSS, at(1, 1))
+                .assertThat(CROSS, at(1, 1));
     }
 
     @Test(expected = PositionAlreadyFilledException.class)
@@ -38,30 +37,30 @@ public class TicTacGameShould {
 
     @Test
     public void playNought() throws WrongMoveException {
-        TicTacGame ticTacGame = new TicTacGame()
+        new TicTacGame()
                 .play(CROSS, at(1, 1))
-                .play(NOUGHT, at(1, 2));
-        ticTacGame.assertThat(NOUGHT, at(1, 2));
+                .play(NOUGHT, at(1, 2))
+                .assertThat(NOUGHT, at(1, 2));
     }
 
     @Test
     public void playCrossSecondTurn() throws WrongMoveException {
-        TicTacGame ticTacGame = new TicTacGame()
+        new TicTacGame()
                 .play(CROSS, at(2, 2))
                 .play(NOUGHT, at(1, 1))
-                .play(CROSS, at(1, 2));
-        ticTacGame.assertThat(CROSS, at(1, 2));
+                .play(CROSS, at(1, 2))
+                .assertThat(CROSS, at(1, 2));
     }
 
     @Test(expected = PlayCrossFirstException.class)
     public void notAllowNoughtToPlayFirst() throws WrongMoveException {
-        TicTacGame ticTacGame = new TicTacGame()
+        new TicTacGame()
                 .play(NOUGHT, at(1, 1));
     }
 
     @Test(expected = PlayCrossException.class)
     public void notAllowNoughtToPlayTwice() throws WrongMoveException {
-        TicTacGame ticTacGame = new TicTacGame()
+        new TicTacGame()
                 .play(CROSS, at(1, 1))
                 .play(NOUGHT, at(1, 2))
                 .play(NOUGHT, at(1, 3));
@@ -69,7 +68,7 @@ public class TicTacGameShould {
 
     @Test(expected = PlayNoughtException.class)
     public void notAllowCrossToPlayTwice() throws WrongMoveException {
-        TicTacGame ticTacGame = new TicTacGame()
+        new TicTacGame()
                 .play(CROSS, at(1, 1))
                 .play(CROSS, at(1, 2));
     }
