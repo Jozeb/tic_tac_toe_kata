7ca02ceb24423d053b5bed01c155a84fb826e7d1
lamak <lamak.qaizar@careem.com>
2020-04-19 05:57:30 +0500

    REFACTOR: removed unused isNought method

--- src/main/java/board/Marker.java
+++ src/main/java/board/Marker.java
@@ -3,10 +3,6 @@ package board;
 public enum Marker {
     CROSS, NOUGHT, EMPTY;
 
-    public boolean isNought() {
-        return NOUGHT == this;
-    }
-
     public boolean isNotEmpty() {
         return EMPTY != this;
     }
