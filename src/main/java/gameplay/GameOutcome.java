6addf946bb9dcbe430bd410afe38631b5d3b8a54
Jazib Hassan <jazib.hassan@careem.com>
2020-04-19 22:40:35 +0500

    GREEN Implemented whenGameHasEndedAsDraw logic

--- src/main/java/gameplay/GameOutcome.java
+++ src/main/java/gameplay/GameOutcome.java
@@ -1,6 +1,11 @@
 package gameplay;
 
 import board.Board;
+import board.Marker;
+import java.util.Arrays;
+import position.Column;
+import position.Position;
+import position.Row;
 import win.WinValidator;
 
 import java.util.Optional;
@@ -9,17 +14,31 @@ import static gameplay.GameState.IN_PROGRESS;
 
 public class GameOutcome {
 
-    GameState gameState = IN_PROGRESS;
-    WinValidator winValidator = new WinValidator();
+  GameState gameState = IN_PROGRESS;
+  WinValidator winValidator = new WinValidator();
 
-    public void updateBasedOn(Board board) {
-        Optional<GameState> outcome = winValidator.isWin(board);
-        if (outcome.isPresent()) {
-            this.gameState = outcome.get();
-        }
+  public void updateBasedOn(Board board) {
+    Optional<GameState> outcome = winValidator.isWin(board);
+    if (outcome.isPresent()) {
+      this.gameState = outcome.get();
     }
 
-    public GameState getGameState() {
-        return gameState;
+    long filledBoxesCount = Arrays.stream(Row.values())
+        .map(row -> Arrays.stream(Column.values())
+            .map(column -> Position.at(row, column))
+            .map(board::whatIsAt)
+            .filter(Marker::isNotEmpty)
+            .count()).mapToInt(Math::toIntExact)
+        .sum();
+
+    if (filledBoxesCount == 9) {
+        this.gameState = GameState.DRAWN;
     }
+
+  }
+
+  public GameState getGameState() {
+    return gameState;
+  }
+
 }
