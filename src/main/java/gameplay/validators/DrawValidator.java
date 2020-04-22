adb0ad22a84463303c407e5bb986ce105108d4cf
lamak <lamak.qaizar@careem.com>
2020-04-23 04:50:33 +0500

    REFACTOR: removed redirection in draw validator

--- src/main/java/gameplay/validators/DrawValidator.java
+++ src/main/java/gameplay/validators/DrawValidator.java
@@ -13,23 +13,21 @@ import java.util.stream.Stream;
 import static gameplay.GameState.DRAWN;
 
 public class DrawValidator implements BoardValidator {
-    @Override
-    public boolean isGameFinished(Board board) {
-        return isBoardFull(board);
-    }
 
     @Override
     public GameState getGameState() {
         return DRAWN;
     }
 
-    private boolean isBoardFull(Board board) {
+    @Override
+    public boolean isGameFinished(Board board) {
         return enumeratePositions().map(board::whatIsAt)
                 .noneMatch(Marker::isEmpty);
     }
 
     private Stream<Position> enumeratePositions() {
         return Arrays.stream(Row.values())
-                .flatMap(row -> Arrays.stream(Column.values()).map(column -> Position.at(row, column)));
+                .flatMap(row -> Arrays.stream(Column.values())
+                        .map(column -> Position.at(row, column)));
     }
 }
